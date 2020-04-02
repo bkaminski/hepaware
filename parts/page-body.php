a4ac11ce041a8dcea1759dc223e85cd16cdb7b91
Ben Kaminski <ben@benkaminski.com>
2020-04-02 12:14:04 -0400

    added page styles and tags

--- parts/page-body.php
+++ parts/page-body.php
@@ -1,7 +1,12 @@
 <div class="int-body">
-	<div class="container mainbody">
-
-
-
+	<div class="container mainbody pt-3">
+		<?php the_content(); ?>
+			
+	</div>
+	<div class="container-fluid no-padding">
+		<div class="col-md-12 pt-3 pb-3 tag-area text-white">
+			<?php the_tags('<span class="badge badge-light mr-2"><i class="fas fa-tags"></i> Topics:</span>  ', ', '); ?>
+				
+		</div>
 	</div>
 </div>
