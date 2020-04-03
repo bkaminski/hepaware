3d3fe2250f3f1b56038f12171214248e146540a2
Ben Kaminski <ben@benkaminski.com>
2020-04-03 10:03:33 -0400

    cleaned up footer on mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,4 +3,19 @@
 //REMOVE TITLE TAG FROM LINK HOVER
 	$('.nav-link').removeAttr('title');
 
+// Swap alignment at mobile
+    var $window = $(window);
+         function checkWidth() {
+            if ($window.width() < 574) {
+                $('.donate--footer').addClass('text-center pt-3');
+            };
+            if ($window.width() >= 574) {
+                $('.donate--footer').removeClass('text-center pt-3');
+            }
+        }
+        // Execute on load
+        checkWidth();
+        // Bind event listener
+$(window).resize(checkWidth); 
+
 })( jQuery );
--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="ha-footer pb-2">
 	<div class="container pt-5">
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col col-sm-6 col-md-4">
 				<h4 class="text-white">Contact</h4>
 				<address>
 					HepAware<br />
@@ -11,7 +11,7 @@
 				</address>
 
 			</div>
-			<div class="col-md-4 footer-links">
+			<div class="col col-sm-6 col-md-4 footer-links">
 				<h4 class="text-white">Helpful Links</h4>
 					<ul>
 						<li>Information</li>
@@ -21,7 +21,7 @@
 						<li>Resources</li>
 					</ul>
 			</div>
-			<div class="col-md-4">
+			<div class="col-xs-12 col-sm-12 col-md-4 donate--footer">
 				<h4 class="text-white pb-3">Donate</h4>
 				<a class="btn btn-outline-red2 btn-lg pl-5 pr-5 rounded-0" href="#" role="button">Donate Now <i class="fas fa-angle-double-right fa-fw fa-lg"></i></a>
 			</div>
